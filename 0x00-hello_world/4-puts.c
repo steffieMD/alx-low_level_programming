e1a6e074147611a57c079ad44cdea0e417cd066b
“steffieMD” <stephaniemetieh@gmail.com>
2023-02-16 15:00:45 -0800

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -9,7 +9,6 @@
  */
 int main(void)
 {
-	/* prints programming stuff*/
-	puts("/"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
