4e13ca705035187da5fe188107b7e7fed2acb296
“steffieMD” <stephaniemetieh@gmail.com>
2023-02-16 15:39:11 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,7 +16,7 @@ int main(void)
 	printf("Size of a char: %ld byte(s)\n", sizeof(char_var));
 	printf("Size of an int: %ld byte(s)\n", sizeof(int_var));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(longint_var));
-	printf("Size of an long long int: %ld byte(s)\n", sizeof(longlongint_var));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(longlongint_var));
 	printf("Size of a float: %ld byte(s)\n", sizeof(float_var));
 
 	return (0);
