dabbf9cd1acfe25a4bf81c43ba674540e1f0094c
“steffieMD” <stephaniemetieh@gmail.com>
2023-02-16 14:45:20 -0800

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
-
+	/**
+	 * This is the preferred style for multi-line
+	 * Puts(prints out).
+	 * Return statement.
+	 */
 int main(void) 
 {
+	/* Prints something */
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return(0);
 }
