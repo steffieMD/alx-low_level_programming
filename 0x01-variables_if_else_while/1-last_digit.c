e978bcfa1f9eecf859335bb522551c3d3f285762
“steffieMD” <stephaniemetieh@gmail.com>
2023-02-17 11:41:07 -0800

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,13 +9,11 @@
  */
 int main(void)
 {
-	int n;
+	int n, last_digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	int last_digit
-	
+	/* your code goes there */	
 	last_digit = n % 10;
 	if (last_digit > 5)
 	{
