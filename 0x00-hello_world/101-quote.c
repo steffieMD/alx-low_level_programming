70226b234a5535f2ee2c7b9078cf3d2e21470590
“steffieMD” <stephaniemetieh@gmail.com>
2023-02-16 16:01:36 -0800

    quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+nclude<stdio.h>
+/**
+ * Main - entry point
+ * result - 0
+ */
+int main(void)
+{
+	write(1,"and that piece of art is useful\" - Dora Korpar, 2015-10-19", strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+	return (1);
+}
