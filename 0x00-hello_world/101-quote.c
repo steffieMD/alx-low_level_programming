911a9d519c1f769ef978b09ed3476cf010168e2c
“steffieMD” <stephaniemetieh@gmail.com>
2023-02-16 16:13:42 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
  * Main - entry point
  * result - 0
  */
-size_t strlen(const char *s);
+size_t strlen(const char *s)
 {
 	write(1,"and that piece of art is useful\" - Dora Korpar, 2015-10-19", strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 	return (1);
