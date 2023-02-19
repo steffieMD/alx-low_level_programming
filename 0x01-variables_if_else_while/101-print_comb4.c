d0ef80cff5a3a519c963eee07e3d360846caba13
“steffieMD” <stephaniemetieh@gmail.com>
2023-02-19 13:27:13 -0800

    print comb 3 digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: This program prints all the numbers of base 16 in lowercase
+ * Return: Always 0 (SUccess)
+ * Author: SteffieMD
+ */
+
+int main(void)
+{
+	int x, y, z;
+
+	for (x = 0; x < 8; x++)
+	{
+		for (y = x + 1; y < 9; y++)
+		{
+			for (z = y + 1; z < 10; z++)
+			{
+				putchar('0' + x);
+				putchar('0' + y);
+				putchar('0' + z);
+				
+				if (x == 7 && y == 8 && z == 9)
+				continue;
+
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
