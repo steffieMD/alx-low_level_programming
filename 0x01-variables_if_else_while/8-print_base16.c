0240210d722e27e225464cddf2c653bec262fe9a
“steffieMD” <stephaniemetieh@gmail.com>
2023-02-19 11:46:29 -0800

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: This program prints all the numbers of base 16 in lowercase
+ * Return: Always 0 (SUccess)
+ * Author: SteffieMD
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; nu < 10; num++)
+	{
+		putchar('0' + num);
+	}
+	putchar('\n');
+	return 0;
+}
