9a3839a9babd95936ea2dba0077a2ecfbb361f59
“steffieMD” <stephaniemetieh@gmail.com>
2023-02-19 12:45:48 -0800

    print comb double digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: This program prints all the numbers of base 16 in lowercase
+ * Return: Always 0 (SUccess)
+ * Author: SteffieMD
+ */
+
+int main(void)
+{
+	int hex;
+
+	for (hex = 0; hex < 10; hex++)
+	{
+		putchar('0' + hex);
+		putchar('0' + hex);
+		if (hex < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
