ae76e54d34454944529cf9bc132f4962151d0daa
“steffieMD” <stephaniemetieh@gmail.com>
2023-02-19 10:37:21 -0800

    a-z minus q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: This program prints the alphabet in lowercase,
+ * It also excludes q and e.
+ * Return: Always 0 (Success)
+ * Author: SteffieMD
+ */
+int main(void)
+{
+	char lc;
+	
+	for (lc = 'a'; lc <= 'z'; lc++)
+	{
+		if (lc != 'e' && lc != 'q')
+		{
+			putchar(lc);
+		}
+		putchar('\n');
+	}
+	return (0)
+}
