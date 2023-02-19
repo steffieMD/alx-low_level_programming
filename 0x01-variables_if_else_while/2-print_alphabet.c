088ed6eae9feaf33f826b993a912ae484c2cf087
“steffieMD” <stephaniemetieh@gmail.com>
2023-02-19 09:15:22 -0800

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: This program prints the alphabet in lowercase.
+ * Return: Always 0 (Success)
+ * Author: SteffieMD
+ */
+int main(void)
+{
+	char lowercase_alpha;
+
+	for(lowercase_alpha = 'a'; lowercase_alpha <= 'z'; lowercase_alpha++)
+	{
+		putchar(lowercase_alpha);
+	}
+	return(0);
