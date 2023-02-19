5d0a08d8520f57f692e3d6fa4ac015ccad1eb129
“steffieMD” <stephaniemetieh@gmail.com>
2023-02-19 10:58:33 -0800

    print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ * Description: this program prints all single digit numbers of base 10
+ * starting from 0
+ * Return: Always 0 (Success)
+ * Author: SteffieMD
+ */
+
+int main(void)
+{
+	int base10;
+
+	for (base10 = 0; base10 < 10; base10++)
+	{
+		printf("%d", base10);
+	}
+	printf("\n");
+	return (0);
+}
