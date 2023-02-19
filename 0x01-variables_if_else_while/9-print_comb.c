dbd21ecd288b26478f6a74bdcfc37788bdb8f0d2
“steffieMD” <stephaniemetieh@gmail.com>
2023-02-19 12:20:51 -0800

    print comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,10 +11,13 @@ int main(void)
 {
 	int hex;
 
-	for (hex = 0; hex <= 9; hex++)
+	for (hex = 0; hex < 10; hex++)
 	{
 		putchar('0' + hex);
-		putchar(',');
+		if (hex < 9)
+		{
+			putchar(', ');
+		}
 	}
 	putchar('\n');
 	return (0);
