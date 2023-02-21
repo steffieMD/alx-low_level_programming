1520fbc1632688d72d7856b3ad59a097681a2516
“steffieMD” <stephaniemetieh@gmail.com>
2023-02-21 12:34:43 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,20 +5,20 @@
  * main - Entry point
  * Description: prints _putchar, followed by a new line.
  * Return: Always 0 (Success)
- * Author: IanoNjuguna
+ * Author: SteffieMD
  */
 
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-
-return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	
+	return (0);
 }
