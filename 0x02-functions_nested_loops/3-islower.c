e49b93788fe37d8d974aace80e05ed7377668ce3
“steffieMD” <stephaniemetieh@gmail.com>
2023-02-21 13:49:14 -0800

    is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _islower - Entry point
+ *
+ * Return: Always 0
+ */
+
+int _islower(int c)
+{
+	char s;
+
+	if (_islower(s))
+	{
+		_putchar('1' + s);
+	}
+	else
+	{
+		_putchar('0' + s);
+	}
+	
+	_putchar('\n');
+	return (0);
+}
