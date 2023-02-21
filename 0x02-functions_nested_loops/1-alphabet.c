08456beebea4f3e66b93096d72f6c90864aa85b1
“steffieMD” <stephaniemetieh@gmail.com>
2023-02-21 12:46:09 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+
+/**
+ * print_alphabet - Entry point
+ * Description: This program prints the alphabet, in lowercase, followed by a new line. 
+ * Return: Always 0 (Success)
+ * Author: SteffieMD
+ */
+
+void print_alphabet(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet < 'z'; alphabet++)
+	{
+		_putchar('alphabet');
+	}
+	_putchar('\n');
+	return (0);
+}
+
