79eebd0e6022131bc90d05c1f8f2acfe7a4425ce
“steffieMD” <stephaniemetieh@gmail.com>
2023-02-21 14:56:03 -0800

    fibonacci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,32 +1,26 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * purpose - fibonacci
- *
- * Return: (0) always
+ * main - Prints the sum of Even Fibonacci numbers
+ * less than 4000000.
+ * Return: Nothing!
  */
+
 int main(void)
+
 {
-unsigned long int lim = 4000000;
-unsigned long int bef = 0;
-unsigned long int aft = 1;
-unsigned long int R = 0;
-unsigned long int sum = 0;
 
-															while (lim > R)
-																		{
-															R = bef + aft;
+	int i = 0;
+	long j = 1, k = 2, sum = k;
 
-															if ((R % 2) == 0)
-															{
-																														sum += R;
-																														}
-
-															bef = aft;
-																														aft = R;
-																																	}
-															printf("%li\n", sum);
-															return (0);
+	while (k + j < 4000000)
+	{
+	k += j;
+	if (k % 2 == 0)
+	sum += k;
+	j = k - j;
+	++i;
+	}
+	printf("%ld\n", sum);
+	return (0);
 }
