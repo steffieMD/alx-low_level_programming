7f49bf649bb451848b0d9451fd4ef7eeeffa6245
“steffieMD” <stephaniemetieh@gmail.com>
2023-02-21 14:20:57 -0800

    is alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -9,12 +9,24 @@
 
 int _isalpha(int c)
 {
-	if (c >= 65 && <= 122)
+	/* ASCII value of upper case letters is between 65 && 90 */
+	if (c >= 65 && c <= 90)
 	{
 		return (1);
 	}
+
 	else
 	{
-		return (0);
+		/* ASCII value of lower case letters is between 97 && 122 */
+		if (c >= 97 && c <= 122)
+		{
+			return (1);
+		}
+
+		else
+		{
+			return (0);
+		}
 	}
 }
+
