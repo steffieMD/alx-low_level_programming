dddbe051e83adee420feecdd45e2b2a576378ac2
“steffieMD” <stephaniemetieh@gmail.com>
2023-02-21 14:08:58 -0800

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isalpha - ENtry point
+ * @c: Contains value to be compared
+ * Return - ALways 0
+ * Author: STeffieMD
+ */
+
+int _isalpha(int c);
+{
+	if (c > 65 && <= 122)
+	{
+		_return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
