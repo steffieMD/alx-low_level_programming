99d71b2326c427aca7e032f0685aad665fd9c9b1
“steffieMD” <stephaniemetieh@gmail.com>
2023-02-21 10:27:41 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: This program prints _putchar.
+ * Return: Always 0 (Success)
+ * Author: SteffieMD
+ */
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
