22909216b54124dbf20f4a41a4177ca05596f074
“steffieMD” <stephaniemetieh@gmail.com>
2023-02-21 13:18:00 -0800

    alphabet x 10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -15,7 +15,7 @@ void print_alphabet_x10(void)
 
 	while (count < 10)
 	{
-		for (alphabet = 'a'; alphabet <= 'z'; alphabet ++)
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 		{
 			_putchar(alphabet);
 		}
