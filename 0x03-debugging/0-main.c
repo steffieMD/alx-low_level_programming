6660286a0eb9d12cef03000850260795b8b08776
“steffieMD” <stephaniemetieh@gmail.com>
2023-02-22 08:46:55 -0800

    multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * main - Entry point
- * Return - void
- * Author - STeffieMD
+ * main - tests function that prints if integer is positive or negative 
+ * Return: 0
  */
 
 int main(void)
@@ -13,5 +12,5 @@ int main(void)
 	i = 0;
 	positive_or_negative(i);
 
-	return(0);
+	return (0);
 }
