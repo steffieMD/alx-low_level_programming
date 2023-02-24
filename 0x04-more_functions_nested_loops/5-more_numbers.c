685bbc7b4eb79ce6abf8b7b1fde656d57f7ee804
“steffieMD” <stephaniemetieh@gmail.com>
2023-02-24 03:08:06 -0800

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * more_numbers - prints more numbers
+ * Retuen: Always 0
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+	int first_number, second_number;
+
+	first_number = j / 10;
+	second_number = j % 10;
+
+	for (i = 0; i < 11; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			first_number = j / 10;
+			second_number = j % 10;
+			if (j < 10)
+			{
+				putchar(second_number + '0');
+			}
+			else
+			{
+				putchar(first_number + '0');
+				putchar(second_number + '0');
+			}
+		}
+		putchar('\n');
+	}
+}
