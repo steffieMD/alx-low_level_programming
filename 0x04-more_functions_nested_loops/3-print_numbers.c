dbe6bce752627665039bc2586b281de28b9cd052
“steffieMD” <stephaniemetieh@gmail.com>
2023-02-24 00:37:13 -0800

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,17 @@
 #include "main.h"
+
 /**
  * print_numbers - prints numbers
- * Return: Always 0
+ * Return: void
  */
 
-void print_numbers(void);
+void print_numbers(void)
 {
 	int n;
 
 	for (n = 0; n < 10; n++)
 	{
-		_putchar (n + '0');
+		_putchar('0' + n);
 	}
-	_putchar ('\n');
+	_putchar('\n');
 }
