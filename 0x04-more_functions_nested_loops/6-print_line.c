dc3ff5b18bbfd6d7deb5622d9270e62061a9977a
“steffieMD” <stephaniemetieh@gmail.com>
2023-02-24 06:04:43 -0800

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_line - prints line.
+ * @n: prints int
+ * Return: Always 0.
+ */
+
+void print_line(int n)
+{
+	int n;
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			putchar(95);
+		}
+		putchar('\n');
+	}
+	else
+	{
+		putchar('\n');
+	}
+}
