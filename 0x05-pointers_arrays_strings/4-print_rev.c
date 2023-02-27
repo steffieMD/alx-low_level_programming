2faed64dd27c5f72dc1cd51156b406f58060f1e9
“steffieMD” <stephaniemetieh@gmail.com>
2023-02-27 14:01:59 -0800

    string reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_rev - prints string in reverse
+ * @s: string
+ *
+ * Return: Nothing
+ */
+
+void print_rev(char *s)
+{
+	printf("%s\n", strrev(s));
+}
