ef9742aa9f74b9af405d3e4415ff0c0f145e9210
“steffieMD” <stephaniemetieh@gmail.com>
2023-02-27 13:39:40 -0800

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - prints a string to stdout
+ * @str: char
+ *
+ * Return: Nothing
+ */
+
+void _puts(char *str)
+{
+	int s = puts(str);
+
+	return (s);
+}
