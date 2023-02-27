74a1a35126e559eb8b9ab52948b57b2cdfce09fc
“steffieMD” <stephaniemetieh@gmail.com>
2023-02-27 13:21:48 -0800

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - determines string length
+ * @s: char
+ *
+ * Return: Always 0
+ */
+
+int _strlen(char *s)
+{
+	int len = strlen(s);
+
+	return (len);
+}
