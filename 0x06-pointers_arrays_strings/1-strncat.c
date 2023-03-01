e3cbb2ffb12ee72a2ea78415191567cde7df0509
“steffieMD” <stephaniemetieh@gmail.com>
2023-03-01 09:12:11 -0800

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,20 +11,14 @@ char *_strncat(char *dest, char *src, int n);
 
 char *_strncat(char *dest, char *src, int n)
 {
-	char *str = strncat(dest, src, n);
+       	int dest_len = strlen(dest);
+	int i;
 
-	return(str);
-} 
+	for (i = 0 ; i < n && src[i] != '\0' ; i++)
+	{
+		dest[dest_len + i] = src[i];
+	}
+	dest[dest_len + i] = '\0';
 
-char *_strncat(char *dest, char *src, int n)
-{
-    int dest_len = strlen(dest);
-    int i;
-
-    for (i = 0 ; i < n && src[i] != '\0' ; i++)
-    {
-        dest[dest_len + i] = src[i];
-    }
-    dest[dest_len + i] = '\0';
-    return dest;
+	return dest;
 }
