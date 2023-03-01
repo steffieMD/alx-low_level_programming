5e834269fcdf9c1829b8a683fa62860af257f8b8
“steffieMD” <stephaniemetieh@gmail.com>
2023-03-01 09:30:44 -0800

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,7 +12,15 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	char *str = strncpy(dest, src, n);
+	int i;
 
-	return (str);
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	for ( ; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
 }
