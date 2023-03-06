2910a9a3c82b643f9f122253de6ad33f2ccb4d00
“steffieMD” <stephaniemetieh@gmail.com>
2023-03-06 11:47:16 -0800

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: pointer to string to be scanned
+ * @accept: pointer to string with characters to match
+ * Return: Always 0.
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int *len;
+
+	len = strspn(s, accept);
+
+	return (len);
+}
+
