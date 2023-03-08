e67f78a6ff2d6726f40a63e1a5c2fa10c1fbc7b9
“steffieMD” <stephaniemetieh@gmail.com>
2023-03-08 09:52:20 -0800

    puts recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: a string
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	char *out;
+
+	out = puts(s);
+
+	return (out);
+}
