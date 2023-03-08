4ab45a1d3e7c25368719fa5a04e2afd1f1248e0f
“steffieMD” <stephaniemetieh@gmail.com>
2023-03-08 12:18:20 -0800

    pow recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -13,5 +13,12 @@ int _pow_recursion(int x, int y)
 	{
 		return (-1);
 	}
-	return (x * _pow_recursion(x, y-1));
+	if (y)
+	{
+		return (x * _pow_recursion(x, y-1));
+	}
+	else
+	{
+		return (1);
+	}
 }
