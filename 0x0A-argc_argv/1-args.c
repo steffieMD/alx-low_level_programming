fb870db19919b04aa8e8c18eee507dc06675cf6d
“steffieMD” <stephaniemetieh@gmail.com>
2023-03-18 04:47:56 -0700

    args

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -11,12 +11,6 @@
 int main(int argc, char *argv[])
 {
 	(void) argv; /*Ignore argv*/
-
-	int i;
-
-	for (i = 0; i < argc; i++)
-	{
-		printf("%d\n", i);
-	}
+	printf("%d\n", argc - 1);
 	return (0);
 }
